d24f7063ce7bb8d800a79d44ab95c6f775bd918a
RedFox2017 <15521227263@163.com>
2019-12-29 22:58:47 +0800

    引入MongoDB数据库

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient
 
 var app = new express()
+//配置数据库
+var dbUrl = 'mongodb://localhost:27017/test'
 
 //配置静态托管
 app.use('/static', express.static('statics'))
@@ -11,6 +14,26 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/test', (req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html;charset=UTF8" })
+    MongoClient.connect(dbUrl, (err, client) => {
+        if (err) {
+            res.write('数据库连接失败')
+            return
+        }
+        res.write('数据库连接成功')
+        var db = client.db('test')
+        db.collection('user').insertOne({ "name": "hao" }, (err, result) => {
+            if (err) {
+                res.write('数据库写入失败')
+                return
+            }
+            res.write('数据库写入成功')
+
+            res.end()
+        })
+    })
+})
 //应用级中间件
 app.use((req, res, next) => {
     console.log(new Date())
