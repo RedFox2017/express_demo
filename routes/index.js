08178634d0f2fc2bc955861a9523ab2824f631e2
RedFox2017 <15521227263@163.com>
2019-12-30 23:34:00 +0800

    修改首页和增加个人中心页面

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,16 @@ var express = require('express')
 var router = express.Router()
 var Db = require('../modules/db')
 router.get('/', (req, res) => {
-    Db.find('user', {}, (error, result) => {
+    Db.find('article', {}, (error, result) => {
         res.render('index', {
             username: req.session.username,
             list: result
         })
     })
-
 })
-
+router.get('/person', (req, res) => {
+    res.render('person')
+})
 var user = require('./index/user')
 router.use('/user', user)
 
