71ea93726447e3727e42579ebdcf86dd212a909c
RedFox2017 <15521227263@163.com>
2020-01-21 09:27:51 +0800

    文章api

--- routes/api/article.js
+++ routes/api/article.js
@@ -6,7 +6,16 @@ router.get('/', (req, res) => {
         res.send(result)
     })
 })
-
+router.post('/publish', (req, res) => {
+    req.body.status = 'publish'
+    Db.insertOne('article', req.body, (err, result) => {
+        if (err) {
+            console.log('发布失败')
+        } else {
+            console.log('发布成功')
+        }
+    })
+})
 router.get('/search', (req, res) => {
     console.log(req.query)
     Db.search('article', { status: 'publish', queryString: req.query.title }, (error, result) => {
